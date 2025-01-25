d1f5a7b62377c9386f85197628ec1858c48fda05
Chris <christian-boer@hotmail.com>
2025-01-25 03:03:56 +0100

    Player death and killbox

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+using System;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public event Action OnPlayerDeath;
+    private bool isDead = false;
+    private CharacterController characterController;
+
+    void Awake() {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    public void Die()
+    {
+        if (isDead) return;
+        isDead = true;
+        
+        Debug.Log("Player died!");
+        OnPlayerDeath?.Invoke();
+    }
+
+    public void Respawn(Vector3 spawnPosition)
+    {
+        TeleportPlayer(spawnPosition);
+        isDead = false;
+    }
+
+    public void TeleportPlayer(Vector3 position) {
+        characterController.enabled = false;
+        transform.position = position;
+        characterController.enabled = true;
+    }
+}
