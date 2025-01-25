eb3c2815a01184570e5614d2f8e68e0fa97a99d7
Niels van Dam <nielsvdam@hotmail.com>
2025-01-25 12:52:27 +0100

    show egg velocity

--- Assets/Scripts/Egg.cs
+++ Assets/Scripts/Egg.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System;
+using UnityEngine.PlayerLoop;
 
 public class Egg : MonoBehaviour
 {
@@ -11,18 +12,28 @@ public class Egg : MonoBehaviour
 
     private Rigidbody rb;
 
+    [SerializeField]
+    private float breakSpeedThreshold = 0.1f;
+    private bool isBreaking = false;
+    private float aliveTime = 0f;
+
+    [SerializeField]
+    private float minimumTimeBeforeSpeedCheck = 0.2f;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        aliveTime = 0f;
     }
 
     public void Break(Action onBreak = null)
     {
+        isBreaking = true;
         // TODO: play break animation
         onBreak?.Invoke();
 
         OnBreak?.Invoke();
+        isBreaking = false;
     }
 
     public void Launch(float bulletForce)
@@ -33,5 +44,19 @@ public class Egg : MonoBehaviour
     public void SetCanRebirth(bool v)
     {
         canRebirth = v;
+    }
+
+    void Update()
+    {
+        if (rb == null || UIManager.Instance == null) return;
+        UIManager.Instance.SetEggVelocity(rb.linearVelocity.magnitude);
+
+        aliveTime += Time.deltaTime;
+
+        if (rb.linearVelocity.magnitude < breakSpeedThreshold && !isBreaking && aliveTime > minimumTimeBeforeSpeedCheck)
+        {
+            isBreaking = true;
+            Break();
+        }
     }
 }
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,10 +1,14 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public class UIManager : MonoBehaviour
 {
     [SerializeField]
     private TextMeshProUGUI interactText;
+
+    [SerializeField]
+    private TextMeshProUGUI eggVelocity;
 
     public static UIManager Instance { get; private set; }
 
@@ -23,5 +27,10 @@ public class UIManager : MonoBehaviour
     public void SetInteractText(string text)
     {
         interactText.text = text;
+    }
+
+    public void SetEggVelocity(float velocity)
+    {
+        eggVelocity.text = $"{velocity:F2} m/s";
     }
 }
