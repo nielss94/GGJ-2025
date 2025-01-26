3d8890084255f20c3ccb9972a6c0680efee8776c
Chris <christian-boer@hotmail.com>
2025-01-26 04:12:14 +0100

    Control hints wip

---
+++ Assets/Scripts/UI/ControlsHinter.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class ControlsHinter : MonoBehaviour
+{
+    // Hint show after idle time
+    // Switch icons based on keyboard/controller
+    
+    // WASD movement
+    // Shoot
+    // Teleport
+    // Cancel
+    // Interact
+
+
+
+    private void ShowHints() {
+
+    }
+}
